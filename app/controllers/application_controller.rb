9db4ac043f1d5050924e80b5dbcc76aea640bdf4
Benis Tambe <will.jenifer@yahoo.com>
2021-04-27 16:58:04 -0400

    adding restful routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,39 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  post "/signup" do
+		#your code here!
+		# user = User.new(:username => params[:username], :password => params[:password])
+
+    if user.save
+      redirect "/login"
+    else
+      redirect "/failure"
+    end
+	end
+
+  post "/login" do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/success"
+    else
+      redirect "/failure"
+    end
+  end 
+
 end
+
+
+=begin
+  
+HTTP VERB    ROUTE	          Action	         Used For
+GET	    '/articles'	          index action	  index page to display all articles
+GET    	'/articles/new'       new action  	  displays create article form                        
+POST	  '/articles'  	        create action	  creates one article
+GET    	'/articles/:id'	      show action  	  displays one article based on ID in the url
+GET	    '/articles/:id/edit'	edit action	    displays edit form based on ID in the url
+PATCH	  '/articles/:id'	      update action	  modifies an existing article based on ID in the url
+PUT	    '/articles/:id'	      update action	  replaces an existing article based on ID in the url
+DELETE	'/articles/:id'	      delete action	  deletes one article based on ID in the url  
+=end
