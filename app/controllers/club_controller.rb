49ea1a725ef30d33c0f6ad047932e3e2a8450e3d
Benis Tambe <will.jenifer@yahoo.com>
2021-04-27 17:30:37 -0400

    changed too club routes

--- app/controllers/club_controller.rb
+++ app/controllers/club_controller.rb
@@ -2,18 +2,44 @@ class ClubController < ApplicationController
 
 
     #index action
-    get '/articles' do
-        @articles = Article.all
+    get '/clubs' do
+        @clubs = club.all
         erb :index
     end 
 
     #new action
-    get '/articles/new' do
+    get '/clubs/new' do
         erb :new
     end
       
-    post '/articles' do
-        @article = Article.create(:title => params[:title], :content => params[:content])
-        redirect to "/articles/#{@article.id}"
+    post '/clubs' do
+        @club = Club.create(:title => params[:title], :content => params[:content])
+        redirect to "/clubs/#{@club.id}"
     end 
+
+    #show action
+    get '/clubs/:id' do
+        @club = Club.find_by_id(params[:id])
+        erb :show
+    end 
+
+    #edit action
+    get '/clubs/:id/edit' do  #load edit form
+        @club = Club.find_by_id(params[:id])
+        erb :edit
+    end
+    
+    patch '/clubs/:id' do #edit action
+      @club = Club.find_by_id(params[:id])
+      @club.title = params[:title]
+      @club.content = params[:content]
+      @club.save
+      redirect to "/clubs/#{@club.id}"
+    end 
+
+    delete '/clubs/:id' do #delete action
+        @club = Club.find_by_id(params[:id])
+        @club.delete
+        redirect to '/clubs'
+      end 
 end
